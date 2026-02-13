1fdc864a299f4d1f2a2f491cde02c24af112eed2
aadiwarrior79 <aadiwarrior79@gmail.com>
2026-02-14 05:23:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+const answers_no = {
+    english: [
+        "No",
+        "Are you sure?",
+        "Are you really sure??",
+        "Are you really realy sure???",
+        "Think again?",
+        "Don't believe in second chances?",
+        "Why are you being so cold?",
+        "Maybe we can talk about it?",
+        "I am not going to ask again!",
+        "Ok now this is hurting my feelings!",
+        "You are now just being mean!",
+        "Why are you doing this to me?",
+        "Please give me a chance!",
+        "I am begging you to stop!",
+        "Ok, Let's just start over.."
+    ],
+   
+};
+
+const noGifs = [
+    "1.gif",
+    "2.gif",
+    "3.gif",
+    "4.gif",
+    "5.gif",
+    "6.gif",
+    "7.gif"
+];
+
+let gifIndex = 0;
+
+
+answers_yes = {
+    "english": "Yes",
+}
+
+let language = "english"; // Default language is English
+const no_button = document.getElementById('no-button');
+const yes_button = document.getElementById('yes-button');
+let i = 1;
+let size = 50;
+let clicks = 0;
+
+no_button.addEventListener('click', () => {
+    // Change banner source
+    let banner = document.getElementById('banner');
+   // Cycle through gifs
+    banner.src = noGifs[gifIndex];
+    refreshBanner();
+
+    gifIndex++;
+
+    if (gifIndex >= noGifs.length) {
+    gifIndex = 0;
+}
+
+
+    clicks++;
+    // increase button height and width gradually to 250px
+    const sizes = [40, 50, 30, 35, 45]
+    const random = Math.floor(Math.random() * sizes.length);
+    size += sizes[random]
+    yes_button.style.height = `${size}px`;
+    yes_button.style.width = `${size}px`;
+    let total = answers_no[language].length;
+    yes_button.style.fontSize = `${size / 4}px`;
+    // change button text
+    if (i < total - 1) {
+        no_button.innerHTML = answers_no[language][i];
+        i++;
+    } else if (i === total - 1) {
+        alert(answers_no[language][i]);
+        i = 1;
+        no_button.innerHTML = answers_no[language][0];
+        yes_button.innerHTML = answers_yes[language];
+        yes_button.style.height = "";
+        yes_button.style.width = "";
+        yes_button.style.fontSize = "";
+        yes_button.style.transform = "";
+        banner.src = "mid.gif";
+        refreshBanner();
+        gifIndex = 0; // reset gif cycle
+        size = 50;
+    }
+});
+
+yes_button.addEventListener('click', () => {
+    // change banner gif path
+    let banner = document.getElementById('banner');
+    banner.src = "yes.gif";
+    refreshBanner();
+    //hide question heading
+    let question_heading = document.getElementById('question-heading');
+    question_heading.style.display = "none";
+
+    // hide buttons div
+    let buttons = document.getElementsByClassName('buttons')[0];
+    buttons.style.display = "none";
+    // show message div
+    let message = document.getElementsByClassName('message')[0];
+    message.style.display = "block";
+    message.style.fontSize = "20px";
+
+    // Confetti burst
+  confetti({
+    particleCount: 200,
+    spread: 100,
+    origin: { y: 0.6 }
+  });
+});
+
+function refreshBanner() {
+    // Reload banner gif to force load  
+    let banner = document.getElementById('banner');
+    let src = banner.src;
+    banner.src = '';
+    banner.src = src;
+}
+
